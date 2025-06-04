27e163341833804d7e0d36c918552cee1adcf353
VfdimMGIT <almaz_oven@mail.ru>
2025-06-05 00:44:05 +0500

    Update config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+THE_CAT_API_KEY = os.getenv("THE_CAT_API_KEY")
+NASA_API_KEY = os.getenv("NASA_API_KEY")
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+
